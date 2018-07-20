e7b0d440c1e794e3ff03696cd9aa9a08fc8eee35
Kamilo Amir <kamilo@gmail.com>
2018-07-20 14:51:05 -0400

    Install EventGen Script
    Created install_eventgen.sh script to expidite the install of eventgen.

---
+++ install_eventgen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#prompt to install event-gen 
+echo "Do you want to install the event-gen on your instance?"
+read yneventgen
+if  [[ "$yneventgen" == *y* ]];then
+	#open "https://s3.amazonaws.com/splunk-bash-install/eventgen-develop.tgz"
+    wget https://s3.amazonaws.com/splunk-bash-install/eventgen-develop.tgz
+	#echo "Downloading file https://s3.amazonaws.com/splunk-bash-install/eventgen-develop.tgz"
+	echo "Downloaded eventget to" pwd
+#setup eventgen
+echo "What is the Splunk instance you want to install the eventget into?"
+read splunkhome
+
+tar xzvf eventgen-develop.tgz -C "/opt/splunk/splunk_$splunkhome/etc/apps"
+echo "Splunk Eventgen installed."
+fi
