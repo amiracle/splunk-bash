d8c6cca85d282490b30f3fdc02eb00f5b00c5ac8
Kamilo Amir <kamilo@gmail.com>
2020-09-09 17:26:22 -0400

    Update copytos3.sh

--- copytos3.sh
+++ copytos3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 sync . s3://splunk-bash-install --exclude '.git*' --exclude 'copytos3.sh' --acl public-read
+aws s3 sync . s3://splunk-bash-installs --exclude '.git*' --exclude 'copytos3.sh' --acl public-read
